33b65edf8525982985d9d11df4d1dcfb80e8c4cf
Matiss Janis Aboltins <matiss@mja.lv>
2017-04-23 22:55:16 +0300

    Run? please

--- docs/main.js
+++ docs/main.js
@@ -1,14 +1,12 @@
-import { Customer } from './js/classes/customer';
-import { Engine } from './js/engine';
-
 $(function() {
   $('form').on('submit', function (event) {
     event.preventDefault();
 
     var grossSalary = $('input[name="salary"]').val();
 
-    const grossSalary = 200000;
-    const engine = new Engine(new Customer(grossSalary));
-    engine.run();
+    SystemJS.import(["classes/customer", "engine"]).then(function (Customer, Engine) {
+      var engine = new Engine(new Customer(grossSalary));
+      engine.run();
+    });
   });
 }());
